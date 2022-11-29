2131137ef4e704fdcfb1fd59014066d6b9a2377c
danibalo <daniyemak@gmail.com>
2022-11-29 13:54:40 -0800

    make upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/bash/python3
+def uppercase(str):
+    """prints a string in uppercase followed by a new line."""
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print()
