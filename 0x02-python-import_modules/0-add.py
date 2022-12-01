6a12aa5b27c6ebc5f45822189d4151a4ce3a7739
danibalo <daniyemak@gmail.com>
2022-12-01 03:58:43 -0800

    importing add function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 from add_0 import add
-add(1, 2)
+add(a, b)
--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add (a, b):
-    """ Addas integers at variables a and b"""
+    """ Adds integers at variables a and b"""
     a, b = 1, 2
     c = a + b
     print("{} + {} = {}".format(a, b, c), end="")
