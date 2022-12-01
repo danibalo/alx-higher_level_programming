3bef92dd4dddd23c67a83985f4dd7199e861fc50
danibalo <daniyemak@gmail.com>
2022-12-01 03:38:16 -0800

    rm

--- 0x02-python-import_modules/add_0.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def add(a, b):
-    """adds the integers at variable a and b"""
-    c = a + b
-    print("{} + {} = {}".format(a, b, c), end="")
-import add from add_0
-add(1, 2)
