5dc01b6e351ac11e7021a31b163d14a1beb77c77
danibalo <daniyemak@gmail.com>
2023-01-04 08:03:18 -0800

    a function that creates a copy of the string, removing the character at the position n

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,7 +2,6 @@
 def remove_char_at(str, n):
     for i in range(len(str)):
         if i == n:
-            str = str[:i] + str[i + 1:]
-            return str
+            return str[:n] + str[n + 1:]
         elif n > len(str) or n < 0:
             return str
