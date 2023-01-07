6b5785488897ab19b96846b210c100bc550bb896
danibalo <daniyemak@gmail.com>
2023-01-07 14:28:35 -0800

    infinite addition of commandline arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """ prints the result of the addition of all arguments"""
-    import sys
+if __name__ == '__main__':
 
-    i = len(sys.argv) - 1
+    """ a program that prints the result of the addition of all arguments """
+    import sys
+    i = 1
     sum = 0
-    for j in range(i):
-        sum = sum + int(sys.argv[j + 1])
-    print("{}".format(sum))
+    while(i < len(sys.argv)):
+        sum += int(sys.argv[i])
+        i += 1
+    print(sum)
