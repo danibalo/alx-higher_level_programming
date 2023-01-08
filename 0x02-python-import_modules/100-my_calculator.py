f529b5d0b3053422741e786f0414026c88fd1bb5
danibalo <daniyemak@gmail.com>
2023-01-08 01:19:25 -0800

    a program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,9 +3,9 @@ if __name__ == '__main__':
 
     import sys
     from calculator_1 import add, sub, mul, div
-    if len(sys.argv) < 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
+    if len(sys.argv) != 4:
+         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+         exit(1)
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
