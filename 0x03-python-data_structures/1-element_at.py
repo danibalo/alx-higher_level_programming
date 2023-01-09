e3c36878c88d6325533f233c3d39ebfc352f062e
danibalo <daniyemak@gmail.com>
2023-01-09 12:35:27 -0800

    a function that retrieves an element from a list like in C.

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
+    """My function that prints lest element at given index"""
+    number_of_element = len(my_list)
+    if idx < 0 or idx > number_of_element:
         return None
     else:
         return my_list[idx]
