96ffdf48439f50dd2f280902d4ea938d202a3aa3
danibalo <daniyemak@gmail.com>
2023-01-10 06:19:59 -0800

    filter c or C from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def  no_c(my_string):
     new = ''
-    for i in range(len(my_string) - 1):
-        if my_string[i] == 'c' or my_string[i] == 'C':
-            new = my_string[:i] + my_string[i + 1:]
-            return new
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new += i
+    return new
 
 
