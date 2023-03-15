0cfc00f292c0029fa300d58fc17d78cc81307c31
danibalo <daniyemak@gmail.com>
2023-03-15 07:44:37 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const i = process.argv.length;
 if (i === 2) {
-  console.log('No Argument');
+  console.log('No argument');
 } else if (i === 3) {
   console.log('Argument found');
 } else {
